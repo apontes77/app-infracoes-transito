e50b7b6d744b858932ed9c4dad59453720b62573
apontes77 <alexandrepontesgyn@gmail.com>
2021-05-30 16:30:19 -0300

    adiciona dependencias do retrofit e as configuracoes

---
+++ app/src/main/java/br/com/pucgo/appInfracoes/retrofit/RestApiClient.java
@@ -0,0 +1,36 @@
+package br.com.pucgo.appInfracoes.retrofit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RestApiClient{
+
+    public static Retrofit retrofit = null;
+
+    public static final String BASE_URL = "http://10.0.2.2:8081/api/v1";
+
+    public static Retrofit getClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .build();
+
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+        return retrofit;
+    }
+
+
+}
+
---
+++ app/src/main/java/br/com/pucgo/appInfracoes/retrofit/RestApiInterfaceUsuario.java
@@ -0,0 +1,29 @@
+package br.com.pucgo.appInfracoes.retrofit;
+
+import java.util.List;
+
+import br.com.pucgo.appInfracoes.modelos.Denuncia;
+import br.com.pucgo.appInfracoes.modelos.Usuario;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface RestApiInterfaceUsuario {
+    @Headers("Content-Type: application/json")
+    @POST("/usuario")
+    Call<Usuario> inserirUsuario(
+            @Body Usuario usuario);
+
+    @GET("/usuario")
+    Call<List<Usuario>> listarUsuarios ();
+
+    @GET("/usuario/{id}")
+    Call<Usuario> obterUsuarios(@Query("id") Integer id);
+}
+
