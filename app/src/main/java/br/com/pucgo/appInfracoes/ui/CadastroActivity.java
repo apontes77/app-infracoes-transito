20e43367fa667f4b95d786345f846fc013b96288
apontes77 <alexandrepontesgyn@gmail.com>
2021-06-02 18:11:23 -0300

    adiciona organizacao de validacoes e classe utilitaria

--- app/src/main/java/br/com/pucgo/appInfracoes/ui/CadastroActivity.java
+++ app/src/main/java/br/com/pucgo/appInfracoes/ui/CadastroActivity.java
@@ -13,16 +13,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import br.com.pucgo.appInfracoes.R;
+import br.com.pucgo.appInfracoes.utilidades.GeraToast;
+import br.com.pucgo.appInfracoes.validacoes.ValidacoesDeUsuario;
 
 public class CadastroActivity extends AppCompatActivity {
-    private static final String EMAIL_PATTERN =
-            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-    private static final String SENHA_PATTERN ="^(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-
-    private static final Pattern patternEmail = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
-    private static final Pattern patternSenha = Pattern.compile(SENHA_PATTERN, Pattern.CASE_INSENSITIVE);
 
     Button btn_cadastro_usuario;
     EditText email_cadastro;
@@ -43,13 +37,14 @@ public class CadastroActivity extends AppCompatActivity {
         btn_cadastro_usuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (validarEmail(email_cadastro.getText().toString()) 
-                        && validarSenha(senha_cadastro.getText().toString(), senha_repetida_cadastro.getText().toString())) {
+                if (ValidacoesDeUsuario.validarEmail(email_cadastro.getText().toString())
+                        && ValidacoesDeUsuario.validarSenha(senha_cadastro.getText().toString(), senha_repetida_cadastro.getText().toString())) {
+                    GeraToast.criaToastCurto(getApplicationContext(), "Cadastro Realizado com Sucesso!");
                     Intent voltaParaTelaDeLogin = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(voltaParaTelaDeLogin);
                 }
                 else {
-                    Toast.makeText(CadastroActivity.this, "deu algo errado", Toast.LENGTH_SHORT).show();
+                    GeraToast.criaToastCurto(getApplicationContext(), "Algo deu errado!");
                     limpaCampos();
                 }
 
@@ -61,18 +56,5 @@ public class CadastroActivity extends AppCompatActivity {
         email_cadastro.setText("");
         senha_cadastro.setText("");
         senha_repetida_cadastro.setText("");
-    }
-
-    public static boolean validarEmail(String email){
-        Matcher matcher = patternEmail.matcher(email);
-        return matcher.matches();
-    }
-    
-    public static boolean validarSenha(String senhaInserida, String senhaRepetida) {
-        Matcher matcher = null;
-        if(senhaInserida.equals(senhaRepetida)) { 
-            matcher = patternSenha.matcher(senhaInserida);
-        }
-        return matcher.matches();
     }
 }
---
+++ app/src/main/java/br/com/pucgo/appInfracoes/validacoes/ValidacoesDeUsuario.java
@@ -0,0 +1,29 @@
+package br.com.pucgo.appInfracoes.validacoes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidacoesDeUsuario {
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final String SENHA_PATTERN ="^(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+    private static final Pattern patternEmail = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static final Pattern patternSenha = Pattern.compile(SENHA_PATTERN, Pattern.CASE_INSENSITIVE);
+
+    public static boolean validarEmail(String email){
+        Matcher matcher = patternEmail.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean validarSenha(String senhaInserida, String senhaRepetida) {
+        Matcher matcher = null;
+        if(senhaInserida.equals(senhaRepetida)) {
+            matcher = patternSenha.matcher(senhaInserida);
+        }
+        return matcher.matches();
+    }
+}
