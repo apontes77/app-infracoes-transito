d3ccfbd209fd124e67e796ce1c206b917f3aac73
apontes77 <alexandrepontesgyn@gmail.com>
2021-06-09 00:23:51 -0300

    corrige construtor

--- app/src/main/java/br/com/pucgo/appTrafficViolations/ui/LoginActivity.java
+++ app/src/main/java/br/com/pucgo/appTrafficViolations/ui/LoginActivity.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.gson.Gson;
 
 import br.com.pucgo.appTrafficViolations.R;
 import br.com.pucgo.appTrafficViolations.models.User;
@@ -28,7 +25,7 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
 
     private RestApiInterfaceUser apiServiceUser;
-    public static final String NOME_PREFERENCE = "INFORMACOES_LOGIN_AUTOMATICO";
+    public static final String NAME_PREFERENCES = "INFORMACOES_LOGIN_AUTOMATICO";
     private EditText txtLogin;
     private EditText txtPassword;
     private TextView txtRegister;
@@ -74,7 +71,7 @@ public class LoginActivity extends AppCompatActivity {
             txtPassword.requestFocus();
             return;
         } else if (UserValidations.validateEmail(login) && UserValidations.validatePassword(password)) {
-            User user = new User(login, password);
+            User user = new User().builder().login(login).password(password).build();
             Call<ResponseBody> callVerifyUser = apiServiceUser.loginUser(user);
 
             callVerifyUser.enqueue(new Callback<ResponseBody>() {
@@ -99,7 +96,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void saveUserDataInSharedPreferences() {
-        SharedPreferences.Editor editor = getSharedPreferences(NOME_PREFERENCE, MODE_PRIVATE).edit();
+        SharedPreferences.Editor editor = getSharedPreferences(NAME_PREFERENCES, MODE_PRIVATE).edit();
 
         editor.putString("login", txtLogin.getText().toString());
         editor.putString("password", txtPassword.getText().toString());
@@ -107,7 +104,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void retrievesUserDataFromSharedPreferences() {
-        SharedPreferences data = getSharedPreferences(NOME_PREFERENCE, MODE_PRIVATE);
+        SharedPreferences data = getSharedPreferences(NAME_PREFERENCES, MODE_PRIVATE);
         txtLogin.setText(data.getString("login", "Insira Login"));
         txtPassword.setText(data.getString("password", "Insira Senha"));
     }
